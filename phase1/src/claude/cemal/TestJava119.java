04147fc81fdfd56e280792372e69398287830a89
itu-itis21-yilmazcem21 <92295915+itu-itis21-yilmazcem21@users.noreply.github.com>
2026-04-25 19:44:48 +0300

    fixed 1 parentheses at test file

--- phase1/src/claude/cemal/TestJava119.java
+++ phase1/src/claude/cemal/TestJava119.java
@@ -7,12 +7,12 @@ class TestJava119 {
         List<Boolean> correct = Arrays.asList(
                 s.matchParens(Arrays.asList("()(", ")")).equals("Yes"),
                 s.matchParens(Arrays.asList(")", ")")).equals("No"),
-                s.matchParens(Arrays.asList("(()(()))", "())()))")).equals("No"),
+                s.matchParens(Arrays.asList("(()(())", "())())")).equals("No"),
                 s.matchParens(Arrays.asList(")())", "(()()(")).equals("Yes"),
                 s.matchParens(Arrays.asList("(())))", "(()())((")).equals("Yes"),
                 s.matchParens(Arrays.asList("()", "())")).equals("No"),
                 s.matchParens(Arrays.asList("(()(", "()))()")).equals("Yes"),
-                s.matchParens(Arrays.asList("((((", "((()))")).equals("No"),
+                s.matchParens(Arrays.asList("((((", "((())")).equals("No"),
                 s.matchParens(Arrays.asList(")(()", "(()(")).equals("No"),
                 s.matchParens(Arrays.asList(")(", ")(")).equals("No"));
         if (correct.contains(false)) {
