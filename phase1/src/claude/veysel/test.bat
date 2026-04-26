2f8244be0e6265c6768b98346aefa0c2628dd312
vey5el <veysel.cel@gmail.com>
2026-04-26 06:23:04 +0300

    Created a test.bat file for easier batch test for 11 test files.

---
+++ phase1/src/claude/veysel/test.bat
@@ -0,0 +1,22 @@
+javac Java102_chooseNum.java TestJava102.java
+java TestJava102
+javac Java15_stringSequence.java TestJava15.java
+java TestJava15
+javac Java16_countDistinctCharacters.java TestJava16.java
+java TestJava16
+javac Java18_howManyTimes.java TestJava18.java
+java TestJava18
+javac Java23_strlen.java TestJava23.java
+java TestJava23
+javac Java27_flipCase.java TestJava27.java
+java TestJava27
+javac Java32_findZero.java TestJava32.java
+java TestJava32
+javac Java34_uniqueSorted.java TestJava34.java
+java TestJava34
+javac Java48_isPalindrome.java TestJava48.java
+java TestJava48
+javac Java87_getRow.java TestJava87.java
+java TestJava87
+javac Java93_encode.java TestJava93.java
+java TestJava93
